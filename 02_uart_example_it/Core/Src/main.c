6a1d87bcda1e9c71d7d0bf9d535a58d1b4ff6746
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-07-27 02:47:24 +0100

    uart interrupt based rxing project init

---
+++ 02_uart_example_it/Core/Src/main.c
@@ -0,0 +1,88 @@
+#include <string.h>
+#include "stm32f4xx_hal.h"
+#include "main.h"
+
+
+void SystemClockConfig(void);
+void UART2_Init(void);
+void Error_handler(void);
+uint8_t convert_to_capital(uint8_t data);
+
+UART_HandleTypeDef huart2;
+
+char *user_data = "The application is running\r\n";
+
+int main(void)
+{
+  HAL_Init();
+  SystemClockConfig();
+  UART2_Init();
+
+  uint16_t len_of_data = strlen(user_data);
+  HAL_UART_Transmit(&huart2, (uint8_t*)user_data, len_of_data, HAL_MAX_DELAY);
+
+  uint8_t receive_data;
+  uint8_t data_buffer[100];
+  uint32_t count = 0;
+
+  while(1)
+  {
+  	HAL_UART_Receive(&huart2, &receive_data, 1, HAL_MAX_DELAY);
+  	if (receive_data == '\r')
+  	{
+  		break;
+  	}
+  	else
+  	{
+  		data_buffer[count++] = convert_to_capital(receive_data);
+  	}
+  }
+
+  data_buffer[count++] = '\r';
+
+  HAL_UART_Transmit(&huart2, data_buffer, count, HAL_MAX_DELAY);
+
+
+  while(1);
+
+  return 0;
+}
+
+void SystemClockConfig(void)
+{
+
+}
+
+void UART2_Init(void)
+{
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+  	// There's a problem
+  	Error_handler();
+  }
+}
+
+uint8_t convert_to_capital(uint8_t data)
+{
+	if (data >= 'a' && data <= 'z')
+	{
+		data = data - ('a' - 'A'); // lookup ASCII table `a`=65 - `A`=97
+	}
+	return data;
+}
+
+void Error_handler(void)
+{
+	// Trap here or blink any LED's to indicate error
+	while(1);
+}
+
+
