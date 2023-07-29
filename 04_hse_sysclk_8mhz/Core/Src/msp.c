a9863fc41223426c27b6834b7736eb9f97e705e1
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-07-29 03:30:17 +0100

    HSE SYSCLK 8Mhz project init

---
+++ 04_hse_sysclk_8mhz/Core/Src/msp.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+void HAL_MspInit(void)
+{
+  // Here we do low level `processor` specific inits.
+  //1. Set up the priority grouping of the ARM Cortex MX processor
+  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+
+  //2. Enable the required System exceptions of the ARM Cortex MX processor
+  SCB->SHCSR |= 0x7 << 16; // enable usage fault, mem fault and bus fault exception
+
+  //3. Configure the priority for the System exceptions
+  HAL_NVIC_SetPriority(MemoryManagement_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(BusFault_IRQn, 0, 0);
+  HAL_NVIC_SetPriority(UsageFault_IRQn, 0, 0);
+}
+
+
+void HAL_UART_MspInit(UART_HandleTypeDef *huart)
+{
+	GPIO_InitTypeDef gpio_uart;
+	// here we do the low level `peripheral` specific inits
+
+	//1. enable the clock for the USART2 and GPIO peripheral
+	__HAL_RCC_USART2_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	//2. do the pin muxing configuration
+	gpio_uart.Pin = GPIO_PIN_2;
+	gpio_uart.Mode = GPIO_MODE_AF_PP;
+	gpio_uart.Pull = GPIO_PULLUP;
+	gpio_uart.Speed = GPIO_SPEED_FREQ_LOW;
+	gpio_uart.Alternate = GPIO_AF7_USART2; // UART2_TX
+	HAL_GPIO_Init(GPIOA, &gpio_uart);
+
+	gpio_uart.Pin = GPIO_PIN_3;
+	HAL_GPIO_Init(GPIOA, &gpio_uart);
+
+	//3. enable the IRQ and set up the priority (NVIC settings)
+	HAL_NVIC_EnableIRQ(USART2_IRQn);
+	HAL_NVIC_SetPriority(USART2_IRQn, 15, 0);
+}
